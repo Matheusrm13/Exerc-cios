8623c5c3747187b1c68d6625feb0e63e7fa3d0ea
MATHEUS RIBEIRO MORAIS <matheus.rmorais3@senacsp.edu.br>
2024-10-01 17:19:03 -0300

    primeiro commit

---
+++ Ex 1/script.js
@@ -0,0 +1,7 @@
+let contador1 = 5
+
+while (contador1 > 0) {
+    console.log(contador1);
+    contador1 --;
+}
+
---
+++ Ex 3/script.js
@@ -0,0 +1,8 @@
+let contador = 1;
+let soma = 0;
+
+while (contador <= 10 ) {
+   soma += contador;
+   contador++;
+}
+console.log("A soma dos números de 1 a 10: ", soma)
